08c5f068e87f3c6b612d502ad5fdb64e1ca5c2d7
Priyanshu  Padeliya <79414782+priyanshu30122001@users.noreply.github.com>
2024-05-27 02:53:32 +0530

    Update index.jsx

--- src/components/SingleNews/index.jsx
+++ src/components/SingleNews/index.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import './index.css'
 
 function SingleNews({singleNews}) {
-    const {title,description,urlToImage,publishedAt,author,url}= 
+    const {title,description,image,publishedAt,source,url}= 
     singleNews;
     const defaultImageUrl = 'https://euaa.europa.eu/sites/default/files/styles/width_600px/public/default_images/news-default-big.png?itok=NNXAZZTc'
    
   return (  
    
      <div className='news-card'>
-      <img className='news-img' src={urlToImage || defaultImageUrl } alt="img-url" />
+      <img className='news-img' src={image || defaultImageUrl } alt="img-url" />
       <div className='news-content'>
           <p className='news-title'>{title}</p>
           <p className='news-des'>{description}</p>
-          <p className='news-info'>By:{author} | Date:{publishedAt}</p>
+          <p className='news-info'>By:{sourde[0]} | Date:{publishedAt}</p>
           <a className='read-more' href={url} target='_blank'>Read More</a>
       </div>
       
